3dce56017ff857c60d0835478a3dd65cdb88fc9a
Krzysztof Prajs <kprajs@consdata.pl>
2018-09-13 10:06:58 +0200

    proper logging settings

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,7 @@
 container_name=app
 storage_location=${1:-$(pwd)}
 application_port=${2:-5000}
-gradle docker
+gradle clean docker
+docker stop ${container_name}
 docker rm ${container_name}
-docker run -d --name ${container_name} -v ${storage_location}:/logs -p 5000:8080 com.kryszak/springboot-vuejs-starter
+docker run -d --name ${container_name} -v ${storage_location}:/logs -p ${application_port}:8080 com.kryszak/springboot-vuejs-starter
